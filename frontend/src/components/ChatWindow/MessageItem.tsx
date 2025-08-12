2a38281cd736090b67b18a7311e145b5ac03bd57
Ftima Torkamani <ftimwtorkamani@gmail.com>
2025-08-12 21:43:31 +0330

    update message item

--- frontend/src/components/ChatWindow/MessageItem.tsx
+++ frontend/src/components/ChatWindow/MessageItem.tsx
@@ -1,10 +1,11 @@
 import React, { useMemo } from 'react';
 import clsx from 'clsx';
-import type {ChannelRouteType} from '../../api/message.ts';
+import type { ChannelRouteType } from '../../api/message.ts';
 import MessageFile from './MesssageFile.tsx';
 import { App, Dropdown, type MenuProps } from 'antd';
 import { MoreOutlined } from '@ant-design/icons';
 import { hashColor } from '../../utils/ui.ts';
+import { getFullname } from '../../utils/user.ts';
 
 type Props = {
   sender?: UserModel;
@@ -19,7 +20,6 @@ type Props = {
   onDelete?: () => void;
   onForward?: () => void;
   forwardedFrom?: string;
-  
 };
 
 const MessageItem: React.FC<Props> = ({
@@ -59,11 +59,11 @@ const MessageItem: React.FC<Props> = ({
   const items: MenuProps['items'] = [
     ...(isMe && !message.startsWith('Forwarded from') && !files?.length
       ? [
-        {
-          key: 'edit',
-          label: 'Edit',
-        },
-      ]
+          {
+            key: 'edit',
+            label: 'Edit',
+          },
+        ]
       : []),
     {
       key: 'delete',
@@ -75,7 +75,7 @@ const MessageItem: React.FC<Props> = ({
     },
   ];
 
-  const handleMenuClick: MenuProps['onClick'] = ({key}) => {
+  const handleMenuClick: MenuProps['onClick'] = ({ key }) => {
     if (key === 'edit') onEdit?.();
     if (key === 'forward') onForward?.();
     if (key === 'delete')
@@ -100,7 +100,7 @@ const MessageItem: React.FC<Props> = ({
       >
         <div className="absolute top-0 right-0">
           <Dropdown menu={menuProps} trigger={['click']}>
-            <MoreOutlined className="cursor-pointer text-gray-500"/>
+            <MoreOutlined className="cursor-pointer text-gray-500" />
           </Dropdown>
         </div>
 
@@ -109,15 +109,13 @@ const MessageItem: React.FC<Props> = ({
         )}
 
         {forwardedFrom && (
-          <div className="text-xs italic text-gray-500 mb-1">
-            Forwarded from {forwardedFrom}
-          </div>
+          <div className="text-xs italic text-gray-500 mb-1">Forwarded from {forwardedFrom}</div>
         )}
 
         {message && <p className="text-sm mt-1">{message}</p>}
 
         {files?.length ? (
-          files.map((file) => <MessageFile file={file} key={`file-${file.id}`}/>)
+          files.map((file) => <MessageFile file={file} key={`file-${file.id}`} />)
         ) : (
           <></>
         )}
